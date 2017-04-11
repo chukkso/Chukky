6134ff8f5cfda5bf617c8818ac7fde0979a6246e
Chuks <chukso@hotmail.com>
2017-04-11 01:14:12 -0400

    Adding new script for mysql automation

---
+++ testmysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#source https://www.shellhacks.com/mysql-run-query-bash-script-linux-command-line
+echo "Get listing of Databases, users and replication status" && sleep 1
+echo "enter admin user password"
+mysql -uroot -ppasswd -B <<EOF
+show databases;
+select user,host from mysql.user;
+show master status;
+show slave status;
+show processlist;
+EOF 
+
+echo "All Done"
