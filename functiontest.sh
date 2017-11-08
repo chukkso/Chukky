7dc247ded880f3103686769736cc56fb23473c76
Chukwuekwu <chukkso@hotmail.com>
2017-11-07 23:30:55 -0500

    function script test

---
+++ functiontest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Function to get server status
+INTERESTEDDIR=$1
+whatsup () {
+uptime
+======
+echo "$USER is located at $PWD in this $SHELL"
+==============================================
+echo
+ls -lth
+===========
+echo
+ps aux  
+sleep 2
+curl -v chukkystechservices.com
+===============================================
+ls -l $INTERESTEDDIR
+}
+whatsup
